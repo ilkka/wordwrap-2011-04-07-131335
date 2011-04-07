ebd98ece8b7c95542de629f3f62a2081e49e0e97
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-07 15:48:17 +0300

    Raise error for non-numeric params

--- lib/wordwrap.rb
+++ lib/wordwrap.rb
@@ -1,5 +1,8 @@
+class InvalidParameter < Exception
+end
+
 class Wordwrap
   def initialize(wrapcol)
-
+    raise InvalidParameter, "parameter must be a Fixnum" unless wrapcol.class == Fixnum  
   end
 end
