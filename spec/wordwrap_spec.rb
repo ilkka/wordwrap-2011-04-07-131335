50210de0359b5842c1cc2cfbf28ae990307013fa
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-07 15:45:18 +0300

    Again, don't clobber earlier spec

--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -5,6 +5,12 @@ describe Wordwrap do
   describe "new" do
     it "should instantiate" do
       lambda {
+        Wordwrap.new
+      }.should_not raise_exception
+    end
+
+    it "should take an integer parameter" do
+      lambda {
         Wordwrap.new 4
       }.should_not raise_exception
     end
