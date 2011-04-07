f11b95e8aca496c5500f8b7668acfb6aa9ccc7e2
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-07 15:45:58 +0300

    *now* it's proper to clobber

--- spec/wordwrap_spec.rb
+++ spec/wordwrap_spec.rb
@@ -3,16 +3,16 @@ require 'wordwrap'
 
 describe Wordwrap do
   describe "new" do
-    it "should instantiate" do
-      lambda {
-        Wordwrap.new
-      }.should_not raise_exception
-    end
-
     it "should take an integer parameter" do
       lambda {
         Wordwrap.new 4
       }.should_not raise_exception
+    end
+
+    it "should raise an exception for a non-numeric parameter" do
+      lambda {
+        Wordwrap.new
+      }.should raise_exception
     end
   end
 end
